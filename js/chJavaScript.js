f5afb313b9371cbf2bbadef447d75f8110e59720
kaylawessling <43077889+kaylawessling@users.noreply.github.com>
2018-11-15 10:04:48 -0500

    Add files via upload

---
+++ js/chJavaScript.js
@@ -0,0 +1,7 @@
+
+// Burial Options submit button pop-up confirmation alert
+$(document).ready(function(){
+  $('.submitButton').click(function() {
+    alert("Thank you for your submission! Someone will be in contact with you shortly.");
+  });
+});
